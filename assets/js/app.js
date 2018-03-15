950d14296dda2baca3ed8bb9c263956ffb6e0d78
Sebastian Michaelsen <sebastian@michaelsen.io>
2018-03-15 08:21:58 +0100

    [TASK] Add basic css and js for vocabulary cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,22 @@
 require('../css/app.scss');
+
+function ready(fn) {
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+// init revealable cards
+ready(function () {
+    var revealableCards = document.querySelectorAll('.revealable-card');
+    Array.prototype.forEach.call(revealableCards, function (card) {
+        var toggle = card.querySelector('[data-reveal]');
+        toggle.addEventListener('click', function (event) {
+            card.classList.add('revealable-card--revealed');
+            event.preventDefault();
+            event.stopPropagation();
+        });
+    });
+});
