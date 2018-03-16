9ada459167b9fdee2e5da7e8245b6444d692c6da
Sebastian Michaelsen <sebastian@michaelsen.io>
2018-03-16 06:53:10 +0100

    [WIP][TASK] AJAX and pure.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+require('../../node_modules/pure/libs/pure.js');
 require('../css/app.scss');
 require('../images/paper.png');
 
@@ -9,13 +10,52 @@ function ready(fn) {
     }
 }
 
+function getJson(url, onsuccess, onerror) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            var data = JSON.parse(request.responseText);
+            onsuccess(data);
+        } else {
+            onerror();
+        }
+    };
+    request.onerror = function() {
+        onerror();
+    };
+    request.send();
+}
+
 // init revealable cards
 ready(function () {
-    var revealableCards = document.querySelectorAll('.revealable-card');
+    var revealableCards = document.querySelectorAll('[data-revealable]');
     Array.prototype.forEach.call(revealableCards, function (card) {
         var toggle = card.querySelector('[data-reveal]');
         toggle.addEventListener('click', function (event) {
-            card.classList.add('revealable-card--revealed');
+            card.classList.add('revealed');
+            event.preventDefault();
+            event.stopPropagation();
+        });
+    });
+});
+
+// completing cycles
+ready(function () {
+    var loadCycle = function(cycleElement) {
+        getJson(
+            cycleElement.getAttribute('data-cycle-load-url'),
+            function(data) {
+            },
+            function () {
+            }
+        );
+    };
+    var cycles = document.querySelectorAll('[data-cycle]');
+    Array.prototype.forEach.call(cycles, function (cycleElement) {
+        var toggle = card.querySelector('[data-reveal]');
+        toggle.addEventListener('click', function (event) {
+            card.classList.add('revealed');
             event.preventDefault();
             event.stopPropagation();
         });
