4adb99f654e8a4233b20d2692b8ba32ea6ef98f4
Sebastian Michaelsen <sebastian@michaelsen.io>
2018-05-02 22:24:51 +0200

    [FEATURE] Implement /api/subjects/{subjectId} route

--- src/Controller/SubjectController.php
+++ src/Controller/SubjectController.php
@@ -5,12 +5,13 @@ namespace App\Controller;
 use App\Entity\Subject;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SubjectController extends BaseController
 {
     /**
-     * @Route("/api/subjects", name="subjects")
+     * @Route("/api/subjects", name="getAllSubjects")
      * @return Response
      */
     public function getAllSubjects(): Response
@@ -19,5 +20,19 @@ class SubjectController extends BaseController
         return new JsonResponse(['subjects' => array_map(function (Subject $subject) {
             return $subject->getPublicResource();
         }, $subjects)]);
+    }
+
+    /**
+     * @Route("/api/subjects/{subjectId}", name="getSingleSubject")
+     * @return Response
+     */
+    public function getSingleSubject(int $subjectId): Response
+    {
+        $subject = $this->getSubjectRepository()->findOneBy(['id' => $subjectId]);
+        if ($subject instanceof Subject) {
+            return new JsonResponse(['subject' => $subject->getPublicResource()]);
+        } else {
+            throw new NotFoundHttpException('The subject could not be loaded');
+        }
     }
 }
