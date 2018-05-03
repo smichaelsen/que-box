48b6fc0e717241462c2fa4d8ca822da690942df0
Sebastian Michaelsen <sebastian@michaelsen.io>
2018-05-03 20:29:44 +0200

    [FEATURE] Implement updateSubject

--- src/Controller/SubjectController.php
+++ src/Controller/SubjectController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Subject;
+use App\Http\NoContentResponse;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -46,39 +47,73 @@ class SubjectController extends BaseController
     public function createSubject(Request $request): Response
     {
         $data = \json_decode($request->getContent(), true);
-
-        // fill model with data
-        /** @var Subject $subject */
-        $subject = (function () use ($data): Subject {
-            $subject = new Subject();
-            if (isset($data['title'])) {
-                $subject->setTitle($data['title']);
-            }
-            return $subject;
-        })();
-
-        // validate model
-        $valid = (function () use ($subject): bool {
-            // title is not empty
-            if (empty($subject->getTitle())) {
-                return false;
-            }
-            // check for subject with that name
-            $existingSubject = $this->getSubjectRepository()->findOneBy(['title' => $subject->getTitle()]);
-            if ($existingSubject instanceof Subject) {
-                return false;
-            }
-            return true;
-        })();
-
-        if ($valid !== true) {
+        $subject = new Subject();
+        $subject = $this->fillSubjectWithData($subject, $data);
+        if ($this->validateSubject($subject) !== true) {
             return new JsonResponse([], 400);
         }
 
-        // persist model
         $em = $this->getDoctrine()->getManager();
         $em->persist($subject);
         $em->flush();
         return new JsonResponse(['subject' => $subject->getPublicResource()], 201);
+    }
+
+    /**
+     * @Route("/api/subjects/{subjectId}", methods="PATCH")
+     * @param Request $request
+     * @param int $subjectId
+     * @return Response
+     */
+    public function updateSubject(Request $request, int $subjectId): Response
+    {
+        $subject = $this->getSubjectRepository()->findOneBy(['id' => $subjectId]);
+        if ($subject instanceof Subject) {
+            $data = \json_decode($request->getContent(), true);
+            $subject = $this->fillSubjectWithData($subject, $data);
+            if ($this->validateSubject($subject) !== true) {
+                return new JsonResponse([], 400);
+            }
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($subject);
+            $em->flush();
+            return new NoContentResponse();
+        }
+        throw new NotFoundHttpException('The subject could not be loaded');
+    }
+
+    private function fillSubjectWithData(Subject $subject, array $data): Subject
+    {
+        if (isset($data['title'])) {
+            $subject->setTitle($data['title']);
+        }
+        if (isset($data['targetCyclesPerDay'])) {
+            $subject->setTargetCyclesPerDay((int) $data['targetCyclesPerDay']);
+        }
+        return $subject;
+    }
+
+    private function validateSubject(Subject $subject): bool
+    {
+        if (empty($subject->getTitle())) {
+            return false;
+        }
+        // check for subject with that name
+        $queryBuilder = $this->getSubjectRepository()->createQueryBuilder('s');
+        if ($subject->getId() > 0) {
+            $queryBuilder
+                ->where('s.id != :id AND s.title = :title')
+                ->setParameter('id', $subject->getId())
+                ->setParameter('title', $subject->getTitle());
+        } else {
+            $queryBuilder
+                ->where('s.title = :title')
+                ->setParameter('title', $subject->getTitle());
+        }
+        $result = $queryBuilder->getQuery()->getResult();
+        if (isset($result[0]) && $result[0] instanceof Subject) {
+            return false;
+        }
+        return true;
     }
 }
