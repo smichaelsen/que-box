2f067afa0004d6b5b09d5d302a33a24ad5c85862
Sebastian Michaelsen <sebastian@michaelsen.io>
2018-05-03 21:58:56 +0200

    [FEATURE] Implement getScoreCountsOfSubject and getCardsOfSubjectByScore

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -10,13 +10,38 @@ use Symfony\Component\Routing\Annotation\Route;
 class CardController extends BaseController
 {
     /**
-     * @Route("/api/subjects/{subjectId}/cards", methods="GET")
+     * @Route("/api/subjects/{subjectId}/scoreCounts", methods="GET")
      * @param int $subjectId
      * @return Response
      */
-    public function getCardsOfSubject(int $subjectId): Response
+    public function getScoreCountsOfSubject(int $subjectId): Response
     {
-        $cards = $this->getCardRepository()->findBy(['subject' => $subjectId]);
+        $queryBuilder = $this->getCardRepository()->createQueryBuilder('c');
+        $result = $queryBuilder
+            ->select('COUNT(c.id) as count', 'c.score')
+            ->where('c.subject = :subjectId')
+            ->setParameter('subjectId', $subjectId)
+            ->groupBy('c.score')
+            ->orderBy('c.score')
+            ->getQuery()
+            ->getResult();
+        $scoreCounts = [];
+        foreach ($result as $resultItem) {
+            $scoreCounts[$resultItem['score']] = $resultItem['count'];
+        }
+        ksort($scoreCounts);
+        return new JsonResponse(['scoreCounts' => $scoreCounts]);
+    }
+
+    /**
+     * @Route("/api/subjects/{subjectId}/cards/{score}", methods="GET")
+     * @param int $subjectId
+     * @param int $score
+     * @return Response
+     */
+    public function getCardsOfSubjectByScore(int $subjectId, int $score): Response
+    {
+        $cards = $this->getCardRepository()->findBy(['subject' => $subjectId, 'score' => $score]);
         return new JsonResponse(['cards' => \array_map(static function (Card $card) {
             return $card->getPublicResource();
         }, $cards)]);
